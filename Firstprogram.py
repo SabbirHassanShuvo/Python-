ca3d54c4368cd0508e0613b5aa53ad009cfb0d54
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2025-10-10 03:54:25 +0600

    First Program

---
+++ Firstprogram.py
@@ -0,0 +1,87 @@
+from math import remainder
+
+
+print("Hello World")
+
+print("My name is shivam", "I am 18 years old", "I am from Bangladesh")
+
+# variables
+name ="Sabbir"
+age = 18
+price = 34.45
+
+print(name, age, price)
+
+# data types
+print(type(name))
+print(type(age))
+print(type(price))
+
+# comment in python
+
+#single line comment
+#multi line comment
+"""
+This is a multi line comment
+This is a multi line comment
+"""
+
+# typer of oprators
+# 1. arithmetic operators
+a = 10
+b = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a ** b) #abn
+
+# 2. realtional operators
+A = 10
+B = 20
+print(A == B)
+print(A != B)
+print(A > B)
+print(A < B)
+print(A >= B)
+print(A <= B)
+
+# 3. logical operators
+print(A and B)
+print(A or B)
+print(not B)
+
+# 4. assignment operators
+C = 10
+C += 10
+print(C)
+C -= 10
+print(C)
+C *= 10
+print(C)
+C /= 10
+print(C)
+
+# type conversion
+a = 10
+b = 20.5
+c = a + b
+print(c)
+
+# type casting
+a = 10
+b = str(a)
+print(b)
+
+# input python
+# name = input("Enter your name: ")
+# print("Hello", name)
+
+# simple calculator
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("Sum of two numbers is: ", sum)
+

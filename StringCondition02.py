70d917fc8edec605c9309122ba9a836dbb073ea6
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2025-10-12 16:22:13 +0600

    String and conditional statement

---
+++ StringCondition02.py
@@ -0,0 +1,30 @@
+# str = "Hello World"
+# str1 = "I am a student"
+# final = str + str1
+# print(final)
+
+# # length of string
+# print(len(str))
+
+# # string slicing
+# print(str[1:11])
+# print(str[5:])
+
+# string functions
+# str = "hello i am sabbir hassan shuvo im a student"
+# result = str.endswith("student")
+# result = str.capitalize()
+# result = str.replace("student", "developer")
+# result = str.find("student")
+# result = str.count("student")
+# print(result)
+
+
+# Conditional statements
+# if statement
+light = "green"
+
+if light == "green": 
+    print("You can cross the road")
+else:
+    print("You can not cross the road")
